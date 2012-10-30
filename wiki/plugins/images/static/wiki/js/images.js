58cb725faaea29666cae1bb5e722666dad3982d1
benjaoming <benjaoming@gmail.com>
2012-10-30 13:45:39 +0100

    eh..remove alert()

--- wiki/plugins/images/static/wiki/js/images.js
+++ wiki/plugins/images/static/wiki/js/images.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $('.wiki-article .thumbnail').each(function() {
     caption = $(this).children('.caption').html();
-    alert(caption);
     $(this).children('a').colorbox({width:"75%", height:"75%", title: caption})
   });
 });
