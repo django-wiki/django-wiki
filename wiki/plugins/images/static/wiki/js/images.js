ce1ff2e37ecaf6bca10634c29bb5e7e18c17c152
benjaoming <benjaoming@gmail.com>
2012-10-30 13:44:41 +0100

    Setting Colorbox.js width and height (#69) and adding captions.

--- wiki/plugins/images/static/wiki/js/images.js
+++ wiki/plugins/images/static/wiki/js/images.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-  $('.wiki-article .thumbnail a').colorbox();
+  $('.wiki-article .thumbnail').each(function() {
+    caption = $(this).children('.caption').html();
+    alert(caption);
+    $(this).children('a').colorbox({width:"75%", height:"75%", title: caption})
+  });
 });
