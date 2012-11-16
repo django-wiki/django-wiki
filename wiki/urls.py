9604209c5859a5dfa552a4050ab0607706253814
benjaoming <benjaoming@gmail.com>
2012-11-16 22:07:18 +0100

    #71 - missing pattern 'get' in new class based urls

--- wiki/urls.py
+++ wiki/urls.py
@@ -6,7 +6,7 @@ from wiki.views import article, accounts
 from wiki.conf import settings
 from wiki.core.plugins import registry
 
-class WikiURLPatterns( object ):
+class WikiURLPatterns(object):
     '''
     configurator for wiki urls.
     
@@ -39,7 +39,7 @@ class WikiURLPatterns( object ):
     login_view_class = accounts.Login
     logout_view_class = accounts.Logout
 
-    def get_urls( self ):
+    def get_urls(self):
         urlpatterns = self.get_root_urls()
         urlpatterns += self.get_accounts_urls()
         urlpatterns += self.get_revision_urls()
@@ -48,92 +48,93 @@ class WikiURLPatterns( object ):
         urlpatterns += self.get_plugin_urls()
         return urlpatterns
 
-    def get_root_urls( self ):
-        urlpatterns = patterns( '',
-            url( '^$', self.article_view_class.as_view(), name = 'root', kwargs = {'path': ''} ),
-            url( '^create-root/$', self.create_root, name = 'root_create' ),
-            url( '^_search/$', self.search_view_class.as_view(), name = 'search' ),
-            url( '^_revision/diff/(?P<revision_id>\d+)/$', self.article_diff_view, name = 'diff' ),
-        )
+    def get_root_urls(self):
+        urlpatterns = patterns('',
+            url('^$', self.article_view_class.as_view(), name='root', kwargs={'path': ''}),
+            url('^create-root/$', self.create_root, name='root_create'),
+            url('^_search/$', self.search_view_class.as_view(), name='search'),
+            url('^_revision/diff/(?P<revision_id>\d+)/$', self.article_diff_view, name='diff'),
+       )
         return urlpatterns
 
-    def get_accounts_urls( self ):
-        urlpatterns = patterns( '',
-            url( '^_accounts/sign-up/$', self.signup_view_class.as_view(), name = 'signup' ),
-            url( '^_accounts/logout/$', self.logout_view_class.as_view(), name = 'logout' ),
-            url( '^_accounts/login/$', self.login_view_class.as_view(), name = 'login' ),
-            )
+    def get_accounts_urls(self):
+        urlpatterns = patterns('',
+            url('^_accounts/sign-up/$', self.signup_view_class.as_view(), name='signup'),
+            url('^_accounts/logout/$', self.logout_view_class.as_view(), name='logout'),
+            url('^_accounts/login/$', self.login_view_class.as_view(), name='login'),
+           )
         return urlpatterns
 
-    def get_revision_urls( self ):
-        urlpatterns = patterns( '',
+    def get_revision_urls(self):
+        urlpatterns = patterns('',
             # This one doesn't work because it don't know where to redirect after...   
-            url( '^_revision/change/(?P<article_id>\d+)/(?P<revision_id>\d+)/$', self.revision_change_view, name = 'change_revision' ),
-            url( '^_revision/preview/(?P<article_id>\d+)/$', self.article_preview_view_class.as_view(), name = 'preview_revision' ),
-            url( '^_revision/merge/(?P<article_id>\d+)/(?P<revision_id>\d+)/preview/$', self.revision_merge_view, name = 'merge_revision_preview', kwargs = {'preview': True} ),
-            )
+            url('^_revision/change/(?P<article_id>\d+)/(?P<revision_id>\d+)/$', self.revision_change_view, name='change_revision'),
+            url('^_revision/preview/(?P<article_id>\d+)/$', self.article_preview_view_class.as_view(), name='preview_revision'),
+            url('^_revision/merge/(?P<article_id>\d+)/(?P<revision_id>\d+)/preview/$', self.revision_merge_view, name='merge_revision_preview', kwargs={'preview': True}),
+           )
         return urlpatterns
 
-    def get_article_urls( self ):
-        urlpatterns = patterns( '',
+    def get_article_urls(self):
+        urlpatterns = patterns('',
             # Paths decided by article_ids
-            url( '^(?P<article_id>\d+)/$', self.article_view_class.as_view(), name = 'get' ),
-            url( '^(?P<article_id>\d+)/delete/$', self.article_delete_view_class.as_view(), name = 'delete' ),
-            url( '^(?P<article_id>\d+)/deleted/$', self.article_deleted_view_class.as_view(), name = 'deleted' ),
-            url( '^(?P<article_id>\d+)/edit/$', self.article_edit_view_class.as_view(), name = 'edit' ),
-            url( '^(?P<article_id>\d+)/preview/$', self.article_preview_view_class.as_view(), name = 'preview' ),
-            url( '^(?P<article_id>\d+)/history/$', self.article_history_view_class.as_view(), name = 'history' ),
-            url( '^(?P<article_id>\d+)/settings/$', self.article_settings_view_class.as_view(), name = 'settings' ),
-            url( '^(?P<article_id>\d+)/source/$', self.article_source_view_class.as_view(), name = 'source' ),
-            url( '^(?P<article_id>\d+)/revision/change/(?P<revision_id>\d+)/$', self.revision_change_view, name = 'change_revision' ),
-            url( '^(?P<article_id>\d+)/revision/merge/(?P<revision_id>\d+)/$', self.revision_merge_view, name = 'merge_revision' ),
-            url( '^(?P<article_id>\d+)/plugin/(?P<slug>\w+)/$', self.article_plugin_view_class.as_view(), name = 'plugin' ),
-            )
+            url('^(?P<article_id>\d+)/$', self.article_view_class.as_view(), name='get'),
+            url('^(?P<article_id>\d+)/delete/$', self.article_delete_view_class.as_view(), name='delete'),
+            url('^(?P<article_id>\d+)/deleted/$', self.article_deleted_view_class.as_view(), name='deleted'),
+            url('^(?P<article_id>\d+)/edit/$', self.article_edit_view_class.as_view(), name='edit'),
+            url('^(?P<article_id>\d+)/preview/$', self.article_preview_view_class.as_view(), name='preview'),
+            url('^(?P<article_id>\d+)/history/$', self.article_history_view_class.as_view(), name='history'),
+            url('^(?P<article_id>\d+)/settings/$', self.article_settings_view_class.as_view(), name='settings'),
+            url('^(?P<article_id>\d+)/source/$', self.article_source_view_class.as_view(), name='source'),
+            url('^(?P<article_id>\d+)/revision/change/(?P<revision_id>\d+)/$', self.revision_change_view, name='change_revision'),
+            url('^(?P<article_id>\d+)/revision/merge/(?P<revision_id>\d+)/$', self.revision_merge_view, name='merge_revision'),
+            url('^(?P<article_id>\d+)/plugin/(?P<slug>\w+)/$', self.article_plugin_view_class.as_view(), name='plugin'),
+           )
         return urlpatterns
 
-    def get_article_path_urls( self ):
-        urlpatterns = patterns( '',
+    def get_article_path_urls(self):
+        urlpatterns = patterns('',
             # Paths decided by URLs
-            url( '^(?P<path>.+/|)_create/$', self.article_create_view_class.as_view(), name = 'create' ),
-            url( '^(?P<path>.+/|)_delete/$', self.article_delete_view_class.as_view(), name = 'delete' ),
-            url( '^(?P<path>.+/|)_deleted/$', self.article_deleted_view_class.as_view(), name = 'deleted' ),
-            url( '^(?P<path>.+/|)_edit/$', self.article_edit_view_class.as_view(), name = 'edit' ),
-            url( '^(?P<path>.+/|)_preview/$', self.article_preview_view_class.as_view(), name = 'preview' ),
-            url( '^(?P<path>.+/|)_history/$', self.article_history_view_class.as_view(), name = 'history' ),
-            url( '^(?P<path>.+/|)_dir/$', self.article_dir_view_class.as_view(), name = 'dir' ),
-            url( '^(?P<path>.+/|)_settings/$', self.article_settings_view_class.as_view(), name = 'settings' ),
-            url( '^(?P<path>.+/|)_source/$', self.article_source_view_class.as_view(), name = 'source' ),
-            url( '^(?P<path>.+/|)_revision/change/(?P<revision_id>\d+)/$', self.revision_change_view, name = 'change_revision' ),
-            url( '^(?P<path>.+/|)_revision/merge/(?P<revision_id>\d+)/$', self.revision_merge_view, name = 'merge_revision' ),
-            url( '^(?P<path>.+/|)_plugin/(?P<slug>\w+)/$', self.article_plugin_view_class.as_view(), name = 'plugin' ),
-            )
+            url('^(?P<path>.+/|)_create/$', self.article_create_view_class.as_view(), name='create'),
+            url('^(?P<path>.+/|)_delete/$', self.article_delete_view_class.as_view(), name='delete'),
+            url('^(?P<path>.+/|)_deleted/$', self.article_deleted_view_class.as_view(), name='deleted'),
+            url('^(?P<path>.+/|)_edit/$', self.article_edit_view_class.as_view(), name='edit'),
+            url('^(?P<path>.+/|)_preview/$', self.article_preview_view_class.as_view(), name='preview'),
+            url('^(?P<path>.+/|)_history/$', self.article_history_view_class.as_view(), name='history'),
+            url('^(?P<path>.+/|)_dir/$', self.article_dir_view_class.as_view(), name='dir'),
+            url('^(?P<path>.+/|)_settings/$', self.article_settings_view_class.as_view(), name='settings'),
+            url('^(?P<path>.+/|)_source/$', self.article_source_view_class.as_view(), name='source'),
+            url('^(?P<path>.+/|)_revision/change/(?P<revision_id>\d+)/$', self.revision_change_view, name='change_revision'),
+            url('^(?P<path>.+/|)_revision/merge/(?P<revision_id>\d+)/$', self.revision_merge_view, name='merge_revision'),
+            url('^(?P<path>.+/|)_plugin/(?P<slug>\w+)/$', self.article_plugin_view_class.as_view(), name='plugin'),
+            url('^(?P<path>.+/|)$', self.article_view_class.as_view(), name='get'),
+           )
         return urlpatterns
 
-    def get_plugin_urls( self ):
-        urlpatterns = patterns( '', )
+    def get_plugin_urls(self):
+        urlpatterns = patterns('',)
         for plugin in registry.get_plugins().values():
-            slug = getattr( plugin, 'slug', None )
-            plugin_urlpatterns = getattr( plugin, 'urlpatterns', None )
+            slug = getattr(plugin, 'slug', None)
+            plugin_urlpatterns = getattr(plugin, 'urlpatterns', None)
             if slug and plugin_urlpatterns:
-                urlpatterns += patterns( '',
-                    url( '^(?P<article_id>\d+)/plugin/' + slug + '/', include( plugin_urlpatterns ) ),
-                    url( '^(?P<path>.+/|)_plugin/' + slug + '/', include( plugin_urlpatterns ) ),
-                )
+                urlpatterns += patterns('',
+                    url('^(?P<article_id>\d+)/plugin/' + slug + '/', include(plugin_urlpatterns)),
+                    url('^(?P<path>.+/|)_plugin/' + slug + '/', include(plugin_urlpatterns)),
+               )
         return urlpatterns
 
-def get_pattern( app_name = "wiki", namespace = "wiki", url_config_class = None ):
+def get_pattern(app_name="wiki", namespace="wiki", url_config_class=None):
     """Every url resolution takes place as "wiki:view_name".
        You should not attempt to have multiple deployments of the wiki in a
        single Django project.
        https://docs.djangoproject.com/en/dev/topics/http/urls/#topics-http-reversing-url-namespaces
     """
     if url_config_class is None:
-        url_config_classname = getattr( settings, 'URL_CONFIG_CLASS', None )
+        url_config_classname=getattr(settings, 'URL_CONFIG_CLASS', None)
         if url_config_classname is None:
             url_config_class = WikiURLPatterns
         else:
-            url_config_modname, config_classname = url_config_classname.rsplit( '.', 1 )
-            url_config_mod = import_module( url_config_modname )
-            url_config_class = getattr( url_config_mod, config_classname )
+            url_config_modname, config_classname=url_config_classname.rsplit('.', 1)
+            url_config_mod = import_module(url_config_modname)
+            url_config_class = getattr(url_config_mod, config_classname)
     urlpatterns = url_config_class().get_urls()
     return urlpatterns, app_name, namespace
