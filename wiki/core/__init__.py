0f02092a7a3b223023d97f1f1117d142f264ed2e
tschmidt-dev <tschmidt@sacfoodcoop.com>
2013-01-24 15:35:03 -0800

    added MARKDOWN_SAFE_MODE setting

--- wiki/core/__init__.py
+++ wiki/core/__init__.py
@@ -1,9 +1,12 @@
 import markdown
 
+from wiki.conf import settings
+
+
 class ArticleMarkdown(markdown.Markdown):
     
     def __init__(self, article, *args, **kwargs):
-        kwargs['safe_mode'] = "remove"
+        kwargs['safe_mode'] = settings.MARKDOWN_SAFE_MODE 
         markdown.Markdown.__init__(self, *args, **kwargs)
         self.article = article
 
