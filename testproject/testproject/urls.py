2aabc2dc45c16c8d095c099b46a8e80dfd0e6a41
benjaoming <benjaoming@gmail.com>
2013-02-03 14:27:27 +0100

    move macros configuration and do not include django_notify twice in the urlconf

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -8,7 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^notify/', include('django_notify.urls', namespace='notify')),
 )
 
 if settings.DEBUG:
