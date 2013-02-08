51daba0327a522a12c7c59d930e755121c4af302
benjaoming <benjaoming@gmail.com>
2013-02-08 12:23:59 +0100

    add model chart in pdf to the build-sdist process

--- build-sdist.sh
+++ build-sdist.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+echo "Compiling LESS files to CSS"
 ./build-less.sh
+
+echo "Building model chart PDF (needs graphviz)"
+cd testproject/
+./manage.py wikiviz wiki --inheritance | dot -Tpdf -o ../model_chart_wiki.pdf
+cd ..
+
+echo "Building Python source distribution"
 rm -Rf *egg-info/
 python setup.py sdist
+
