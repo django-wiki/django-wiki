548cc819e683c1d13d43943eabb4ab3d4c1adaf1
TomLottermann <TomLottermann@googlemail.com>
2013-03-18 21:33:15 +0100

    complete set of languages

--- wiki/plugins/notifications/util.py
+++ wiki/plugins/notifications/util.py
@@ -7,7 +7,7 @@ def get_title(article):
 def truncate_title(title):
     """Truncate a title (of an article, file, image etc) to be displayed in notifications messages."""
     if not title:
-        return _(u"(None)")
+        return _(u"(none)")
     if len(title) > 25:
         return "%s..." % title[:22]
     return title
