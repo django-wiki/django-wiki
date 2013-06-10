e10d573d496defe3cfb2034818cfbf2ff7097791
Thomas Lottermann <TomLottermann@googlemail.com>
2013-06-10 12:36:29 +0200

    Haystack 2.0 broke some stuff (site did not exist). This is fixed now. Furthermore we can use the highlighter by haystack. It does some stuff nicer than django-wikis

--- wiki/search_indexes.py
+++ wiki/search_indexes.py
@@ -1,16 +1,15 @@
-from haystack.indexes import *
-from haystack import site
+from haystack import indexes
 from wiki import models 
 
 
-class ArticleIndex(SearchIndex):
-    text = CharField(document=True, use_template=True)
-    created = DateTimeField(model_attr='created')
-    modified = DateTimeField(model_attr='modified')
+class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    created = indexes.DateTimeField(model_attr='created')
+    modified = indexes.DateTimeField(model_attr='modified')
     #default because indexing fails with whoosh. see. 
     #http://stackoverflow.com/questions/11995367/how-do-i-use-a-boolean-field-in-django-haystack-search-query
     #https://github.com/toastdriven/django-haystack/issues/382
-    other_read = BooleanField(model_attr='other_read',default=False)
+    other_read = indexes.BooleanField(model_attr='other_read',default=False)
 
     def get_model(self):
         return models.Article
@@ -18,6 +17,3 @@ class ArticleIndex(SearchIndex):
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
-
-site.register(models.Article, ArticleIndex)
-
