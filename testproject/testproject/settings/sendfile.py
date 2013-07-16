fcce3ce9fcc48f32b2d41d55c7d3aae7d3715c84
benjaoming <benjaoming@gmail.com>
2013-07-16 23:26:32 +0200

    cleanup

--- testproject/testproject/settings/sendfile.py
+++ testproject/testproject/settings/sendfile.py
@@ -1,23 +1,11 @@
 from testproject.settings import *
 from testproject.settings.local import *
 
-#Django Haystack
-
 INSTALLED_APPS += ['sendfile']
 
 WIKI_ATTACHMENTS_USE_SENDFILE = True
 
 
 SENDFILE_BACKEND = 'sendfile.backends.development'
-#SENDFILE_URL = '/protected'
-
-# Whoosh backend is completely broken
-# https://github.com/toastdriven/django-haystack/issues/522
-# https://github.com/toastdriven/django-haystack/issues/382
-# https://github.com/toastdriven/django-haystack/issues/447
-#HAYSTACK_CONNECTIONS = {
-#    'default': {
-#        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-#        'PATH': os.path.join(PROJECT_PATH, 'whoosh_index'),
-#    },
-#}
+#SENDFILE_URL = None #Not needed
+#SENDFILE_ROOT = None #Not needed
