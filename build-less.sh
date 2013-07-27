b919d5485729ab2262e5273ec4a1e1a6f254e9bb
benjaoming <benjaoming@gmail.com>
2013-07-27 17:06:49 +0200

    Upgrade to Bootstrap 3 RC1, add font-awesome, lots of refactoring

--- build-less.sh
+++ build-less.sh
@@ -3,5 +3,3 @@
 lessc wiki/static/wiki/bootstrap/less/wiki/wiki-bootstrap.less wiki/static/wiki/bootstrap/css/wiki-bootstrap.css
 lessc -x wiki/static/wiki/bootstrap/less/wiki/wiki-bootstrap.less wiki/static/wiki/bootstrap/css/wiki-bootstrap.min.css
 
-lessc wiki/static/wiki/bootstrap/less/wiki/wiki-bootstrap-responsive.less wiki/static/wiki/bootstrap/css/wiki-bootstrap-responsive.css
-lessc -x wiki/static/wiki/bootstrap/less/wiki/wiki-bootstrap-responsive.less wiki/static/wiki/bootstrap/css/wiki-bootstrap-responsive.min.css
