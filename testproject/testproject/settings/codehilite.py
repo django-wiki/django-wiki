8e77f0676a486b5a2f49000952bf8e080e24458a
benjaoming <benjaoming@gmail.com>
2013-08-08 01:57:33 +0200

    add codehilite note in README and a testproject settings module

---
+++ testproject/testproject/settings/codehilite.py
@@ -0,0 +1,6 @@
+from testproject.settings import *
+from testproject.settings.local import *
+
+# Test codehilite with pygments
+
+WIKI_MARKDOWN_KWARGS = {'extensions': ['codehilite','footnotes', 'attr_list', 'headerid', 'extra', ]}
