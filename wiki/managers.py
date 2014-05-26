a7acc42d05668925a7df3baa7755cda4c11eabc5
benjaoming <benjaoming@gmail.com>
2014-05-26 11:32:38 +0200

    pep8

--- wiki/managers.py
+++ wiki/managers.py
@@ -6,7 +6,7 @@ from mptt.managers import TreeManager
 
 
 class ArticleQuerySet(QuerySet):
-    
+
     def can_read(self, user):
         """Filter objects so only the ones with a user's reading access
         are included"""
@@ -20,7 +20,7 @@ class ArticleQuerySet(QuerySet):
                             (Q(group__user=user) & Q(group_read=True))
                             )
         return q
-    
+
     def can_write(self, user):
         """Filter objects so only the ones with a user's writing access
         are included"""
@@ -34,12 +34,13 @@ class ArticleQuerySet(QuerySet):
                             (Q(group__user=user) & Q(group_write=True))
                             )
         return q
-    
+
     def active(self):
         return self.filter(current_revision__deleted=False)
 
+
 class ArticleFkQuerySetMixin():
-    
+
     def can_read(self, user):
         """Filter objects so only the ones with a user's reading access
         are included"""
@@ -49,12 +50,12 @@ class ArticleFkQuerySetMixin():
             q = self.filter(article__other_read=True)
         else:
             # https://github.com/benjaoming/django-wiki/issues/67
-            q = self.filter(Q(article__other_read=True) |
-                            Q(article__owner=user) |
-                            (Q(article__group__user=user) & Q(article__group_read=True))
-                            ).distinct()
+            q = self.filter(
+                Q(article__other_read=True) | Q(article__owner=user) |
+                (Q(article__group__user=user) & Q(
+                    article__group_read=True))).distinct()
         return q
-    
+
     def can_write(self, user):
         """Filter objects so only the ones with a user's writing access
         are included"""
@@ -64,50 +65,68 @@ class ArticleFkQuerySetMixin():
             q = self.filter(article__other_write=True)
         else:
             # https://github.com/benjaoming/django-wiki/issues/67
-            q = self.filter(Q(article__other_write=True) |
-                            Q(article__owner=user) |
-                            (Q(article__group__user=user) & Q(article__group_write=True))
-                            ).distinct()
+            q = self.filter(
+                Q(article__other_write=True) | Q(article__owner=user) |
+                (Q(article__group__user=user) & Q(
+                    article__group_write=True))).distinct()
         return q
 
     def active(self):
         return self.filter(article__current_revision__deleted=False)
 
+
 class ArticleFkEmptyQuerySetMixin():
+
     def can_read(self, user):
         return self
+
     def can_write(self, user):
         return self
+
     def active(self):
         return self
+
 
 class ArticleFkQuerySet(ArticleFkQuerySetMixin, QuerySet):
     pass
 
+
 class ArticleFkEmptyQuerySet(ArticleFkEmptyQuerySetMixin, EmptyQuerySet):
     pass
 
+
 class ArticleManager(models.Manager):
+
     def get_empty_query_set(self):
         return self.get_query_set().none()
+
     def get_query_set(self):
         return ArticleQuerySet(self.model, using=self._db)
+
     def active(self):
         return self.get_query_set().active()
+
     def can_read(self, user):
         return self.get_query_set().can_read(user)
+
     def can_write(self, user):
         return self.get_query_set().can_write(user)
 
+
 class ArticleFkManager(models.Manager):
+
     def get_empty_query_set(self):
         return ArticleFkEmptyQuerySet(model=self.model)
+
     def get_query_set(self):
         return ArticleFkQuerySet(self.model, using=self._db)
+
     def active(self):
         return self.get_query_set().active()
+
     def can_read(self, user):
         return self.get_query_set().can_read(user)
+
     def can_write(self, user):
         return self.get_query_set().can_write(user)
 
@@ -117,26 +136,34 @@ class URLPathEmptyQuerySet(EmptyQuerySet, ArticleFkEmptyQuerySetMixin):
     def select_related_common(self):
         return self
 
+
 class URLPathQuerySet(QuerySet, ArticleFkQuerySetMixin):
-    
+
     def select_related_common(self):
-        return self.select_related("parent", "article__current_revision", "article__owner")
+        return self.select_related(
+            "parent",
+            "article__current_revision",
+            "article__owner")
+
 
 class URLPathManager(TreeManager):
-    
+
     def get_empty_query_set(self):
         return URLPathEmptyQuerySet(model=self.model)
-    
+
     def get_query_set(self):
         """Return a QuerySet with the same ordering as the TreeManager."""
         return URLPathQuerySet(self.model, using=self._db).order_by(
             self.tree_id_attr, self.left_attr)
-    
+
     def select_related_common(self):
         return self.get_query_set().common_select_related()
+
     def active(self):
         return self.get_query_set().active()
+
     def can_read(self, user):
         return self.get_query_set().can_read(user)
+
     def can_write(self, user):
         return self.get_query_set().can_write(user)
