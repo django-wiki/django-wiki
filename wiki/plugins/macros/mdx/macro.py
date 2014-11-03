9c031255b5e3a9148de76310b632c50cd14607d3
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2014-11-04 00:12:54 +0200

    Add possibility to change the title of the article list (by macro)
    The title of an article list had been fixed to "Article index". Now
    it is possible for wiki users to add custom titles as:
    [article_list title='Articles about something']

--- wiki/plugins/macros/mdx/macro.py
+++ wiki/plugins/macros/mdx/macro.py
@@ -61,12 +61,13 @@ class MacroPreprocessor(markdown.preprocessors.Preprocessor):
                 new_text.append(line)
         return new_text
 
-    def article_list(self, depth="2"):
+    def article_list(self, depth="2", title=_("Article index")):
         html = render_to_string(
             "wiki/plugins/macros/article_list.html",
             Context({
                 'article_children': self.markdown.article.get_children(article__current_revision__deleted=False),
                 'depth': int(depth) + 1,
+                'title': title,
             })
         )
         return self.markdown.htmlStash.store(html, safe=True)
@@ -74,7 +75,8 @@ class MacroPreprocessor(markdown.preprocessors.Preprocessor):
         short_description = _('Article list'),
         help_text = _('Insert a list of articles in this level.'),
         example_code = _('[article_list depth:2]'),
-        args = {'depth': _('Maximum depth to show levels for.')}
+        args = {'depth': _('Maximum depth to show levels for.'),
+                'title': _('Title for the list.'),}
     )
 
     def toc(self):
