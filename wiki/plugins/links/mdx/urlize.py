bfcda5fe4928ad34080708e015f1c7550949b973
Christian Duvholt <cxhristian@gmail.com>
2014-11-12 20:52:03 +0100

    Move future import to the top
    All from __future__ imports have to be at the top of the file.

--- wiki/plugins/links/mdx/urlize.py
+++ wiki/plugins/links/mdx/urlize.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 from __future__ import unicode_literals
 """
 Code modified from:
@@ -40,7 +41,6 @@ Negative examples:
 '<p>del.icio.us</p>'
 
 """
-from __future__ import absolute_import
 
 import markdown
 import re
