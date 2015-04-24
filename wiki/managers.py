91bcd5ea489d436062c743700ee8f8639a3bbe1b
Benjamin Bach <benjaoming@gmail.com>
2015-04-24 04:57:06 +0200

    Sort lower levels in [article_list] alphabetically - fix #253

--- wiki/managers.py
+++ wiki/managers.py
@@ -179,6 +179,8 @@ class URLPathEmptyQuerySet(EmptyQuerySet, ArticleFkEmptyQuerySetMixin):
     def select_related_common(self):
         return self
 
+    def default_order(self):
+        return self
 
 class URLPathQuerySet(QuerySet, ArticleFkQuerySetMixin):
 
@@ -187,6 +189,10 @@ class URLPathQuerySet(QuerySet, ArticleFkQuerySetMixin):
             "parent",
             "article__current_revision",
             "article__owner")
+    
+    def default_order(self):
+        """Returns elements by there article order"""
+        return self.order_by('article__current_revision__title')
 
 
 class URLPathManager(QuerySetCompatMixin, TreeManager):
