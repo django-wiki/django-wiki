745c3e45ccbd1690f7378dcdb19a3923fce8a9b2
Luke Plant <L.Plant.98@cantab.net>
2015-05-14 12:00:34 +0100

    Fixed last commit for Django < 1.8 projects

--- wiki/models/__init__.py
+++ wiki/models/__init__.py
@@ -42,7 +42,9 @@ if not 'django.contrib.sites' in django_settings.INSTALLED_APPS:
     raise ImproperlyConfigured(
         'django-wiki: needs django.contrib.sites in INSTALLED_APPS')
 
-TEMPLATE_CONTEXT_PROCESSORS = []
+# Need to handle Django 1.8 'TEMPLATES', recognizing that users may still be
+# using 1.7 conventions/settings with 1.8.
+TEMPLATE_CONTEXT_PROCESSORS = getattr(django_settings, 'TEMPLATE_CONTEXT_PROCESSORS', [])
 if hasattr(django_settings, 'TEMPLATES'):
     # Django 1.8 compat
     backends = [b for b in django_settings.TEMPLATES if b.get('BACKEND', '') == 'django.template.backends.django.DjangoTemplates']
