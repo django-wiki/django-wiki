042630a47fd9a05bfa354eb88119aaa3e54202db
Benjamin Bach <benjaoming@gmail.com>
2015-09-24 23:22:45 +0200

    add dependency on django_nyt

--- wiki/plugins/notifications/south_migrations/0001_initial.py
+++ wiki/plugins/notifications/south_migrations/0001_initial.py
@@ -9,6 +9,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("django_nyt", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'ArticleSubscription'
         db.create_table('notifications_articlesubscription', (
