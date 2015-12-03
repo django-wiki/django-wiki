1f23aa6d3359fa350e15301df2d2da76d8b695c2
Tomas Babej <tomasbabej@gmail.com>
2015-12-03 21:48:56 +0100

    setup: Remove the ReST readme generation step
    This is no longer necessary, as README.rst is included in the
    repository.

--- setup.py
+++ setup.py
@@ -72,18 +72,6 @@ else:
 packages = find_packages()
 
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert(get_path('README.md'), 'rst')
-    long_description = long_description.split(
-        '<!---Illegal PyPi RST data -->')[0]
-    f = open(get_path('README.rst'), 'w')
-    f.write(long_description)
-    f.close()
-except (IOError, ImportError):
-    # No long description... but nevermind, it's only for PyPi uploads.
-    long_description = ""
-
 setup(
     name="wiki",
     version=VERSION,
