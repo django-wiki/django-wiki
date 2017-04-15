1c4965657edcf82da971aad8fc42b83200c6cdcf
Benjamin Bach <benjamin@overtag.dk>
2017-04-15 21:41:35 +0200

    Documentation for settings

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-from datetime import datetime
+import inspect
 import os
 import sys
+
+from django.utils.html import strip_tags
+from django.utils.encoding import force_text
+
+from datetime import datetime
 
 #
 # django-wiki documentation build configuration file, created by
@@ -45,6 +50,52 @@ sys.path.insert(0, os.path.abspath('../testproject'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings.dev")
 
 django.setup()
+
+
+# Auto list fields from django models - from https://djangosnippets.org/snippets/2533/#c5977
+def process_docstring(app, what, name, obj, options, lines):
+    # This causes import errors if left outside the function
+    from django.db import models
+
+    # Only look at objects that inherit from Django's base model class
+    if inspect.isclass(obj) and issubclass(obj, models.Model):
+        # Grab the field list from the meta class
+        fields = obj._meta.get_fields()
+
+        for field in fields:
+            # Skip ManyToOneRel and ManyToManyRel fields which have no 'verbose_name' or 'help_text'
+            if not hasattr(field, 'verbose_name'):
+                continue
+
+            # Decode and strip any html out of the field's help text
+            help_text = strip_tags(force_text(field.help_text))
+
+            # Decode and capitalize the verbose name, for use if there isn't
+            # any help text
+            verbose_name = force_text(field.verbose_name).capitalize()
+
+            if help_text:
+                # Add the model field to the end of the docstring as a param
+                # using the help text as the description
+                lines.append(u':param %s: %s' % (field.attname, help_text))
+            else:
+                # Add the model field to the end of the docstring as a param
+                # using the verbose name as the description
+                lines.append(u':param %s: %s' % (field.attname, verbose_name))
+
+            # Add the field's type to the docstring
+            if isinstance(field, models.ForeignKey):
+                to = field.rel.to
+                lines.append(u':type %s: %s to :class:`~%s`' % (field.attname, type(field).__name__, to))
+            else:
+                lines.append(u':type %s: %s' % (field.attname, type(field).__name__))
+
+    return lines
+
+
+def setup(app):
+    # Register the docstring processor with sphinx
+    app.connect('autodoc-process-docstring', process_docstring)
 
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
