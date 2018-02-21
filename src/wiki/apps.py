37c5f4a3d2846321d11acb877997ff540c36fb47
Raffaele Salmaso <raffaele@salmaso.org>
2018-02-21 21:58:02 +0100

    Load wiki plugins on app ready

--- src/wiki/apps.py
+++ src/wiki/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.core.checks import register
 from django.utils.translation import gettext_lazy as _
+from wiki.core.plugins.loader import load_wiki_plugins
 
 from . import checks
 
@@ -13,3 +14,4 @@ class WikiConfig(AppConfig):
         register(checks.check_for_required_installed_apps, checks.Tags.required_installed_apps)
         register(checks.check_for_obsolete_installed_apps, checks.Tags.obsolete_installed_apps)
         register(checks.check_for_context_processors, checks.Tags.context_processors)
+        load_wiki_plugins()
