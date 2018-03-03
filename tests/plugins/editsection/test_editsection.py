6e38003e3645e8a9a82d86398b2120811357c102
Frank Loemker <floemker@gmx.de>
2018-03-03 09:53:42 -0500

    Fixed Django 2 error: reverse() is now in django.urls.

--- tests/plugins/editsection/test_editsection.py
+++ tests/plugins/editsection/test_editsection.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, unicode_literals
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django_functest import FuncBaseMixin
 from wiki.models import URLPath
 
