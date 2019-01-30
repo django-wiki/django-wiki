471a8a024153304e84763cd754d769d9fac7ae26
Trevor Peacock <trevor@cbcomputers.com.au>
2019-01-30 14:53:10 +1100

    wrap into single commit

--- src/wiki/checks.py
+++ src/wiki/checks.py
@@ -7,6 +7,7 @@ class Tags:
     required_installed_apps = "required_installed_apps"
     obsolete_installed_apps = "obsolete_installed_apps"
     context_processors = "context_processors"
+    fields_in_custom_user_model = "fields_in_custom_user_model"
 
 
 REQUIRED_INSTALLED_APPS = (
@@ -28,6 +29,12 @@ REQUIRED_CONTEXT_PROCESSORS = (
     ('django.contrib.auth.context_processors.auth', 'E007'),
     ('django.template.context_processors.request', 'E008'),
     ('sekizai.context_processors.sekizai', 'E009'),
+)
+
+FIELDS_IN_CUSTOM_USER_MODEL = (
+    # check function, field fetcher, required field type, error code
+    ('check_user_field', 'USERNAME_FIELD', 'CharField', 'E010'),
+    ('check_email_field', 'get_email_field_name()', 'EmailField', 'E011'),
 )
 
 
@@ -66,6 +73,28 @@ def check_for_context_processors(app_configs, **kwargs):
                 Error(
                     "needs %s in TEMPLATE['OPTIONS']['context_processors']" % context_processor[0],
                     id='wiki.%s' % context_processor[1],
+                )
+            )
+    return errors
+
+
+def check_for_fields_in_custom_user_model(app_configs, **kwargs):
+    errors = []
+    from wiki.conf import settings
+    if not settings.ACCOUNT_HANDLING:
+        return errors
+    import wiki.forms_account_handling
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    for check_function_name, field_fetcher, required_field_type, error_code in FIELDS_IN_CUSTOM_USER_MODEL:
+        function = getattr(wiki.forms_account_handling, check_function_name)
+        if not function(User):
+            errors.append(
+                Error(
+                    '%s.%s.%s refers to a field that is not of type %s' % (User.__module__, User.__name__, field_fetcher, required_field_type),
+                    hint='If you have your own login/logout views, turn off settings.WIKI_ACCOUNT_HANDLING',
+                    obj=User,
+                    id='wiki.%s' % error_code,
                 )
             )
     return errors
---
+++ src/wiki/forms_account_handling.py
@@ -0,0 +1,89 @@
+import random
+import string
+
+import django.contrib.auth.models
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import FieldDoesNotExist
+from django.db.models.fields import CharField, EmailField
+from django.utils.translation import gettext_lazy as _
+from wiki.conf import settings
+
+
+def _get_field(model, field):
+    try:
+        return model._meta.get_field(field)
+    except FieldDoesNotExist:
+        return
+
+
+User = get_user_model()
+
+
+def check_user_field(user_model):
+    return isinstance(_get_field(user_model, user_model.USERNAME_FIELD), CharField)
+
+
+def check_email_field(user_model):
+    return isinstance(_get_field(user_model, user_model.get_email_field_name()), EmailField)
+
+
+# django parses the ModelForm (and Meta classes) on class creation, which fails with custom models without expected fields.
+# We need to check this here, because if this module can't load then system checks can't run.
+CustomUser = User \
+    if (settings.ACCOUNT_HANDLING and check_user_field(User) and check_email_field(User)) \
+    else django.contrib.auth.models.User
+
+
+class UserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Add honeypots
+        self.honeypot_fieldnames = "address", "phone"
+        self.honeypot_class = ''.join(
+            random.choice(string.ascii_uppercase + string.digits)
+            for __ in range(10))
+        self.honeypot_jsfunction = 'f' + ''.join(
+            random.choice(string.ascii_uppercase + string.digits)
+            for __ in range(10))
+
+        for fieldname in self.honeypot_fieldnames:
+            self.fields[fieldname] = forms.CharField(
+                widget=forms.TextInput(attrs={'class': self.honeypot_class}),
+                required=False,
+            )
+
+    def clean(self):
+        cd = super().clean()
+        for fieldname in self.honeypot_fieldnames:
+            if cd[fieldname]:
+                raise forms.ValidationError(
+                    "Thank you, non-human visitor. Please keep trying to fill in the form.")
+        return cd
+
+    class Meta:
+        model = CustomUser
+        fields = (CustomUser.USERNAME_FIELD, CustomUser.get_email_field_name())
+
+
+class UserUpdateForm(forms.ModelForm):
+    password1 = forms.CharField(label="New password", widget=forms.PasswordInput(), required=False)
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(), required=False)
+
+    def clean(self):
+        cd = super().clean()
+        password1 = cd.get('password1')
+        password2 = cd.get('password2')
+
+        if password1 and password1 != password2:
+            raise forms.ValidationError(_("Passwords don't match"))
+
+        return cd
+
+    class Meta:
+        model = CustomUser
+        fields = [CustomUser.get_email_field_name()]
