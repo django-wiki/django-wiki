e3fdd5d9207beed414bc213626f297f30208b469
Mads Jensen <mje@inducks.org>
2019-10-08 15:37:40 +0200

    Simplified travis file a bit. Specified attrs version.

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
