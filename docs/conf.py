3f060be79c00ff212773811312176a01d9f0d5fa
Benjamin Balder Bach <benjamin@overtag.dk>
2023-08-28 11:44:49 +0200

    Remove legacy code in docs/conf.py (#1301)

--- docs/conf.py
+++ docs/conf.py
@@ -188,13 +188,7 @@ linkcheck_ignore = [
 
 # -- Options for HTML output ---------------------------------------------------
 
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-if on_rtd:
-    os.system("sphinx-apidoc --doc-project='Python Reference' -f -o . ../wiki")
-if on_rtd:
-    html_theme = "default"
-else:
-    html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
