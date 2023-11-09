d8f23ed5595ccf429ab649b2c56bb451e1e1ed86
Benjamin Balder Bach <benjamin@overtag.dk>
2023-11-09 23:27:03 +0100

    Updates simplify testproject settings (and fix them) (#1325)
    * Updates simplify testproject settings (and fix them)
    
    * Set `SESSION_COOKIE_SECURE = False` explicitly for dev

--- testproject/testproject/settings/dev.py
+++ testproject/testproject/settings/dev.py
@@ -2,21 +2,24 @@ from .base import *  # noqa @UnusedWildImport
 
 DEBUG = True
 
-
 for template_engine in TEMPLATES:
     template_engine["OPTIONS"]["debug"] = True
 
-
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+# Used by debug_toolbar
 INTERNAL_IPS = ["127.0.0.1"]
+
 ALLOWED_HOSTS = [
     "localhost",
     "0.0.0.0",
     "127.0.0.1",
 ]
-SESSION_COOKIE_DOMAIN = ".localhost"
 
+# Removed.
+# See: https://forum.djangoproject.com/t/why-are-cookie-secure-settings-defaulted-to-false/1133/4
+# and https://github.com/django-wiki/django-wiki/pull/1325
+# SESSION_COOKIE_DOMAIN = ".localhost"
 SESSION_COOKIE_SECURE = False
 
 try:
@@ -26,7 +29,6 @@ try:
         "debug_toolbar.middleware.DebugToolbarMiddleware",
     ]
     INSTALLED_APPS = list(INSTALLED_APPS) + ["debug_toolbar"]
-    INTERNAL_IPS = ("127.0.0.1",)
     DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 except ImportError:
     pass
