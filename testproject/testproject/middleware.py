d4f9f44bb3f7c1fd43ab69a2e365b4181d165ae1
Benjamin Balder Bach <benjamin@overtag.dk>
2024-02-21 21:08:11 +0100

    Demo: Only display demo message once (#1342)
    * Upgrade fixtures for django-nyt 1.4
    
    * Only create demo message once

--- testproject/testproject/middleware.py
+++ testproject/testproject/middleware.py
@@ -13,9 +13,18 @@ class DemoMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        messages.add_message(request, messages.WARNING, MSG)
+        demo_message_exists = False
+        storage = messages.get_messages(request)
+        for message in storage:
+            if str(message) == MSG:
+                demo_message_exists = True
+        storage.used = False
+        if not demo_message_exists:
+            messages.add_message(request, messages.WARNING, MSG)
+
         response = self.get_response(request)
 
         # Code to be executed for each request/response after
